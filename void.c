343acaca4fb032f55e68fe980f34152d0f9e74ca
flpmthss <flpmthss@gmail.com>
2025-08-17 21:20:07 -0300

    studys day 17/08

---
+++ void.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <unistd.h>
+
+// Protótipos das funções
+void obterDados();
+void checarValores();
+void enviarParaBancoDeDados(const char *banco);
+char banco[30];
+
+int main() {
+    printf("Iniciando o processo...\n");
+
+    printf("Me informe seu banco.\n");
+    scanf("%s", banco);
+    sleep(2); // Simula um atraso de 2 segundo
+
+    obterDados();
+    checarValores();
+    enviarParaBancoDeDados(banco);
+
+    // Por main ser ter um operador inteiro, retornamos 0 para indicar sucesso.
+    return 0;
+}
+
+void obterDados() {
+    printf("Obtendo dados...\n");
+    sleep(2); // Simula um atraso de 2 segundos
+}
+
+void checarValores() {
+    printf("Checando valores...\n");
+    sleep(2); // Simula um atraso de 2 segundo
+}
+
+void enviarParaBancoDeDados(const char *banco) {
+    printf("Enviando dados do Banco %s\n", banco);
+}
