e424ff905f56e92606063571e0f1c64293892c74
flpmthss <flpmthss@gmail.com>
2025-09-12 11:37:21 -0300

    Repetição de Reajuste Salarial

---
+++ repeticoes-reajuste-salarial.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/*Desenvolva um programa que leia, inicialmente, a porcentagem de reajuste dos salários dos 
+funcionários de uma empresa. Na sequência, deve ler o salário de cada um dos 50 funcionários, 
+calcular e mostrar o novo salário reajustado, aplicando a porcentagem de ajuste sobre os 
+respectivos salários atuais. Ao final, o maior salário reajustado da empresa deve ser 
+apresentado na tela. */
+
+int main () {
+    float reajuste, salario, novoSalario, maiorSalario;
+    maiorSalario = 0;
+
+    
+}
