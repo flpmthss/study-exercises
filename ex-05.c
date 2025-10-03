ceebbf1ddad230afbf844c80062d89d4a903fcec
flpmthss <flpmthss@gmail.com>
2025-10-03 11:44:22 -0300

    ex-05 att

--- ex-05.c
+++ ex-05.c
@@ -2,32 +2,49 @@
 #include <unistd.h>
 
 int main() {
+    // Declaração de constantes com o tamanho dos vetores
     const int tamNumber = 10, tamList = 5;
-    int number[tamNumber], posicao, posNum, achou, num;
+    // Declaração do vetor e variáveis auxiliares
+    int number[tamNumber], posNum, achou, list;
 
     printf("Digite 10 valores inteiros: \n");
 
-    for (posicao = 0; posicao < tamNumber; posicao++) {
-        scanf("%d", &number[posicao]);
+    // Leitura dos valores para o vetor
+    for (int i = 0; i < tamNumber; i++) {
+        scanf("%d", &number[i]);
     }
+
+    printf("Regitrando valores...\n");
+    sleep(2); // Pausa de 2 segundos para simular o registro dos valores
+
     printf("Digite 5 valores para buscar no vetor: \n");
-    for (posicao = 0; posicao < tamList; posicao++) {
-        scanf("%d", &num);
+    // Laço de repetição para ler os valores a serem buscados
+    for (int i = 0; i < tamList; i++) {
+        scanf("%d", &list);
+    }
+
+    printf("Cruzando Valores...\n");
+    sleep(2);
+
+    for (int i = 0; i < posNum; i++) {
+        // Inicialização das variáveis auxiliares
         achou = 0;
-        posNum = 0;
+        posNum = 0;        
+        // Laço de repetição para buscar o valor no vetor
         while (posNum < tamNumber && achou == 0) {
-            if (num == number[posNum]) {
+            // Verifica se o valor foi encontrado. Caso ache, altera a variável achou para 1
+            if (list == number[posNum]) {
             achou = 1;
             } else {
             posNum++;
-        }
-    }
-        
-        if (achou == 1) {
-            printf("O valor %d foi encontrado na posicao %d do vetor.\n", num, posNum);
-        } else {
-            printf("O valor %d nao foi encontrado no vetor.\n", num);
             }
+        }        
+         //  Condicional para imprimir o resultado da busca
+        if (achou == 1) {
+            printf("O valor %d foi encontrado na posicao %d do vetor.\n", list, posNum);
+        } else {
+            printf("O valor %d nao foi encontrado no vetor.\n", list);
+        }
     }
     return 0;
 }
